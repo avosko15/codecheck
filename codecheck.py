b117613ac59699f09ba219638bf52f00d4fc4945
Anna <73409556+avosko15@users.noreply.github.com>
2022-09-25 15:07:57 -0700

    starting with a rough draft of the operators, im thinking ill do an if to set up conditions

--- codecheck.py
+++ codecheck.py
@@ -1,1 +1,15 @@
-print("hello")
+#Anna Vosko CS361
+#This code is going to run thru some math operators as a Mathbot
+
+def add(num1, num2):
+    print(float(num1) + float(num2))
+
+def subtract(num1, num2):
+    print(float(num1) - float(num2))
+
+def multi(num1, num2):
+    print(float(num1) * float(num2))
+
+def divide(num1, num2):
+    print(float(num1) / float(num2))
+    
