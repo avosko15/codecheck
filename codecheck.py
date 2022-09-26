51513e0ad93b25a3dd2a787c51c79520383d526c
Anna <73409556+avosko15@users.noreply.github.com>
2022-09-25 19:15:15 -0700

    got the massive loop working yay

--- codecheck.py
+++ codecheck.py
@@ -12,4 +12,24 @@ def multi(num1, num2):
 
 def divide(num1, num2):
     print(float(num1) / float(num2))
+
+def prompt():
+    equation = input("SuperMathyBot> ")
+    return equation.split(" ")
     
+while True:
+    equa = prompt()
+    if equa[0] == "add":
+        add(equa[1],equa[2])
+    elif equa[0] == "sub":
+        subtract(equa[1],equa[2])
+    elif equa[0] == "mul":
+        multi(equa[1],equa[2])
+    elif equa[0] == "div":
+        if equa[2] == "0":
+            print("cant divide by zero")
+        else:
+            divide(equa[1],equa[2])
+    elif equa[0] == "quit":
+        break
+    else: print("usage: add|sub|mul|div v0 v1 quit")
